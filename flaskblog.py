91aa05978ac9c38f3e31bb7fb780eb6ce7b76f08
cody-co <codykcovington@gmail.com>
2021-01-13 11:21:49 -0800

    added html and css styling

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Cody-Co',
+		'title': 'Blog 1',
+		'content': 'First post',
+		'date_posted': 'January 13, 2021'
+	},
+	{
+		'author': 'Jango Fett',
+		'title': 'Blog 2',
+		'content': 'The lore of Mando',
+		'date_posted': 'January 14, 2021'
+	}
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
